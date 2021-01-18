bdd394b50cce9aeb900ea23008dad11a98a50606
Thifany Nicastro <thifanynicastro@hotmail.com>
2021-01-18 13:43:52 -0300

    User routes, model, repository, service, migration; TypeORM

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,28 @@
+import { getCustomRepository } from 'typeorm';
+
+import User from '../models/User';
+import UserRepository from '../repositories/UsersRepository';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getCustomRepository(UserRepository);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
